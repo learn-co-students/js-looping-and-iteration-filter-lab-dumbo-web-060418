97b5b9507e43f271769dd620cfc54b0376ec9ded
jtx007 <jamesjacobthomas7@gmail.com>
2018-07-16 19:03:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file
+function findMatching(collection, name) {
+  console.log('\n')
+  console.log('\n')
+  const matchingDrivers = []
+  for (const driver of collection) {
+     if (driver.toLowerCase() === name.toLowerCase()) {
+      matchingDrivers.push(driver)
+    }
+  }
+  return matchingDrivers;
+}
+
+  function fuzzyMatch(collection, letters) {
+    const fuzzyNames = []
+    for (const driver of collection) {
+      if(driver.startsWith(letters)) {
+        fuzzyNames.push(driver)
+      }
+    }
+    return fuzzyNames;
+  }
+
+function matchName(collection, name) {
+  const names = []
+  for (const driver of collection) {
+    if(driver.name === name) {
+      names.push(driver)
+    }
+  }
+  return names;
+}
