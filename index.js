32617c51d60b9c1f63589f80d2a338bd756b9220
Indira Williams <indirajamurath@gmail.com>
2018-07-15 21:08:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(collection, name){
+  const people = [];
+  for (const person of collection) {
+    if (person.toLowerCase() === name.toLowerCase()) {
+      people.push(person);
+    }
+  }
+  return people;
+}
+
+function fuzzyMatch(drivers, letters){
+  const people = [];
+  for (const person of drivers){
+    if (person.startsWith(letters)){
+      people.push(person);
+    }
+  }
+  return people;
+}
+
+function matchName(drivers, name){
+  const newObjects = [];
+  for (const obj of drivers){
+    if (obj.name === name){
+      newObjects.push(obj);
+    }
+  }
+  return newObjects;
+}
