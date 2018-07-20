7222dd99a5c68c8a862929d99ea07aa27152ca33
jtx007 <jamesjacobthomas7@gmail.com>
2018-07-20 13:45:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-// Code your solution in this file
-function findMatching(collection, name) {
-  console.log('\n')
-  console.log('\n')
-  const matchingDrivers = []
-  for (const driver of collection) {
-     if (driver.toLowerCase() === name.toLowerCase()) {
-      matchingDrivers.push(driver)
-    }
-  }
-  return matchingDrivers;
+
+function findMatching(drivers, string) {
+  return drivers.filter( function(driver) {return driver.toLowerCase() === string.toLowerCase() })
 }
 
-  function fuzzyMatch(collection, letters) {
-    const fuzzyNames = []
-    for (const driver of collection) {
-      if(driver.startsWith(letters)) {
-        fuzzyNames.push(driver)
-      }
-    }
-    return fuzzyNames;
-  }
+function fuzzyMatch(drivers, string) {
+  return drivers.filter( function (driver) {return driver.startsWith(string)})
+}
 
-function matchName(collection, name) {
-  const names = []
-  for (const driver of collection) {
-    if(driver.name === name) {
-      names.push(driver)
-    }
-  }
-  return names;
+function matchName (drivers, string) {
+  return drivers.filter (function  (driver) { return driver.name === string})
 }
